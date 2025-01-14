a48057f0770bfc55e2dfcc7b7648caaee02b88e1
dan-limaverde <danielimaverde@gmail.com>
2025-01-14 13:03:49 -0300

    Corrige caminhos absolutos para o Vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/', // Configuração para o domínio raiz
+  base: './', // Configuração para build relativo
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
