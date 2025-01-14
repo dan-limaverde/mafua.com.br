e4ccbb0cb8ac2c2f6616186e7a785d1ef055f8ed
dan-limaverde <danielimaverde@gmail.com>
2025-01-14 12:30:59 -0300

    funfa

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/', // Configuração para o domínio raiz
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
